0dc2b12fe959a80de36fbc07bd83dc558624fe69
Dilyana Dimitrova <65170704+DillyVanilly@users.noreply.github.com>
2020-11-30 21:40:07 +0200

    Add files via upload

---
+++ Java Fundamentals 2020/10. Regular Expressions/Lab/MatchDates.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MatchDates {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String expr = "\\b(?<day>\\d{2})([-.\\/ ])(?<month>[A-Z][a-z]{2})\\2(?<year>\\d{4})\\b";
+        String input = scanner.nextLine();
+
+        Pattern pattern = Pattern.compile(expr);
+        Matcher matcher = pattern.matcher(input);
+
+        while (matcher.find()) {
+            String day = matcher.group("day");
+            String month = matcher.group("month");
+            String year = matcher.group("year");
+
+            System.out.printf("Day: %s, Month: %s, Year: %s%n", day, month, year);
+        }
+    }
+}
