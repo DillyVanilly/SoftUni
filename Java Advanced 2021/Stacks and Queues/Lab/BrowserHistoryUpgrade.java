f25148493fcf68fc38b7745f87a74243e4301471
Dilyana Dimitrova <65170704+DillyVanilly@users.noreply.github.com>
2021-01-20 16:10:59 +0200

    Add files via upload

---
+++ Java Advanced 2021/Stacks and Queues/Lab/BrowserHistoryUpgrade.java
@@ -0,0 +1,37 @@
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class BrowserHistoryUpgrade {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayDeque<String> browserHistory = new ArrayDeque<>();
+        ArrayDeque<String> forwardHistory = new ArrayDeque<>();
+
+        String command;
+
+        while (!"Home".equals(command = scanner.nextLine())) {
+
+            if (command.equals("back")) {
+                if (browserHistory.size() < 2) {
+                    System.out.println("no previous URLs");
+                } else {
+                    forwardHistory.addFirst(browserHistory.peek());
+                    browserHistory.pop();
+                    System.out.println(browserHistory.peek());
+                }
+            } else if (command.equals("forward")) {
+                if (forwardHistory.size() < 1) {
+                    System.out.println("no next URLs");
+                } else {
+                    System.out.println(forwardHistory.peek());
+                    browserHistory.push(forwardHistory.pop());
+                }
+            } else {
+                System.out.println(command);
+                browserHistory.push(command);
+                forwardHistory.clear();
+            }
+        }
+    }
+}
